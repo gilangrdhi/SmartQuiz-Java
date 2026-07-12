a14b748b048f6e17eef35a9649c65d5cc35c478a
Gilang Ardhi Maulana <gilangardhi87@gmail.com>
2026-07-12 18:36:57 +0700

    chore: initial maven project setup

---
+++ src/main/java/com/smartquiz/App.java
@@ -0,0 +1,10 @@
+package com.smartquiz;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("=== Sistem Aplikasi Smartquiz ===");
+        System.out.println("Selamat datang di aplikasi Smartquiz. Silakan pilih opsi berikut:");
+    }
+}
