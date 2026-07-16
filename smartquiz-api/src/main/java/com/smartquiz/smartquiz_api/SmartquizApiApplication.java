3b01332966c71a3945655014b302b927df9e5c98
Gilang Ardhi Maulana <gilangardhi87@gmail.com>
2026-07-16 17:54:17 +0700

    chore: restructure repository to separate CLI and API versions

---
+++ smartquiz-api/src/main/java/com/smartquiz/smartquiz_api/SmartquizApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartquiz.smartquiz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartquizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartquizApiApplication.class, args);
+	}
+
+}
